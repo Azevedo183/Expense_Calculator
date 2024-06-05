3233cc2a9a47ef195041b880daf7956a9fa6ee1e
Azevedo183 <armandojpazevedo@gmail.com>
2024-06-05 09:16:55 +0100

    Add a floating bottun and a pup-up

--- app/src/main/java/com/example/expensecalculator/MainActivity.kt
+++ app/src/main/java/com/example/expensecalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.expensecalculator
 
+import android.icu.text.NumberFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +11,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -31,8 +42,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.expensecalculator.ui.theme.ExpenseCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -60,8 +73,19 @@ fun ExpenseCalculatorLayout() {
     var gasInput by remember { mutableStateOf("") }
     var fuelInput by remember { mutableStateOf("") }
     var internetInput by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
+    val balance = balanceInput.toDoubleOrNull() ?: 0.0
+    val grocery = groceryInput.toDoubleOrNull() ?: 0.0
+    val electricity = electricityInput.toDoubleOrNull() ?: 0.0
+    val water = waterInput.toDoubleOrNull() ?: 0.0
+    val gas = gasInput.toDoubleOrNull() ?: 0.0
+    val fuel = fuelInput.toDoubleOrNull() ?: 0.0
+    val left_over = calculateLeftOver(balance, grocery, electricity, water, gas, fuel)
 
+    if (showDialog) {
+        MinimalDialog(onDismissRequest = { showDialog = false })
+    }
     Column (
         modifier = Modifier
             .padding(40.dp)
@@ -145,6 +169,12 @@ fun ExpenseCalculatorLayout() {
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
+        FloatingButton(
+            onClick = {showDialog = true}
+        )
+
+
+
     }
 }
 
@@ -180,3 +210,39 @@ fun EditNumberField(
         )
     )
 }
+
+@Composable
+fun FloatingButton(onClick: () -> Unit) {
+    ExtendedFloatingActionButton(
+        onClick = { onClick() },
+        icon = { Icon(painter = painterResource(id = R.drawable.calculator_solid), "Calculate") },
+        text = { Text(text = "Calculate") },
+    )
+}
+
+@Composable
+fun MinimalDialog(onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Text(
+                text = "This is a minimal dialog",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+
+
+
+
+
